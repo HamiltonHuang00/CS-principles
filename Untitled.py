ffa94aa349bb6d280fb2cf4d36634a2a47618f9e
HamiltonHuang00 <rhuang@bancroftschool.org>
2016-05-06 09:14:29 -0400

    Python experimental

---
+++ Untitled.py
@@ -0,0 +1,15 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 23 2015, 02:52:03) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+x = input("enter an integer")
+enter an integer
+>>> x = int(x)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    x = int(x)
+ValueError: invalid literal for int() with base 10: ''
+>>> if (x>= 93):
+	print (you have a sunny day)
+ 
